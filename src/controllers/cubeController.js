855d2cb76178d11c21352a64673541d91c64259c
Georgi Karchev <georgi.karchev@gmail.com>
2023-01-24 11:17:29 +0200

    Comment cubeController

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,3 +1,6 @@
+//  this file is the cubeController file and it holds multiple actions regarding the cube asset/model
+
+// this is the getCreateCube action
 exports.getCreateCube = (req, res) => {
     res.render('create');
 }
