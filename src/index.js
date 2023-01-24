47ce8a36fd5fe087c76dea3dfa957fc46df5780f
Georgi Karchev <georgi.karchev@gmail.com>
2023-01-24 11:05:04 +0200

    Add views

--- src/index.js
+++ src/index.js
@@ -1,22 +1,37 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const config = require('./config');
-const setupViewEngine = require('./config/viewEngine');
+const config = require("./config");
+const setupViewEngine = require("./config/viewEngine");
+// const { getCreateCube } = require("./controllers/cubeController");
+const cubeController = require("./controllers/cubeController");
 
 setupViewEngine(app);
 // this line does the same as the upper two lines
 // require('./config/viewEngine')(app);
 
 // setup static assets
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 
-
-app.get('/', (req, res) => {
-    // res.send("Home page");
-    // layout doesn't have to be explicitly defined when using the default/main layout
-    // res.render('home', {layout: 'main'});
-    res.render('home', );
+app.get("/", (req, res) => {
+  // res.send("Home page");
+  // layout doesn't have to be explicitly defined when using the default/main layout
+  // res.render('home', {layout: 'main'});
+  res.render("index");
 });
 
-app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`));
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+// app.get('/create', (req, res) => {
+//     res.render('create');
+// });
+// this works
+// app.get('/create', cubeController.getCreateCube);
+
+// using modular routes
+
+app.listen(config.PORT, () =>
+  console.log(`Server is running on port ${config.PORT}...`)
+);
