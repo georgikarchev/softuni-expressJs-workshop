03309fddc7414caa3eb9b1bdd616cbeeed3026cb
Georgi Karchev <georgi.karchev@gmail.com>
2023-01-24 13:17:54 +0200

    Add 404

--- src/routes.js
+++ src/routes.js
@@ -10,9 +10,11 @@ const homeController = require('./controllers/homeController');
 
 router.get("/", homeController.getHomePage);
 router.get("/about", homeController.getAboutPage);
+router.get('/404', homeController.get404);
 
 router.get('/create', cubeController.getCreateCube);
 router.post('/create', cubeController.postCreateCube);
-router.get('/details/:cubeId', cubeController.getCube);
+router.get('/details/:cubeId', cubeController.getDetails);
+
 
 module.exports = router;
