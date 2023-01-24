295665de956ca0ccd67937a6701e0e43252d83ee
Georgi Karchev <georgi.karchev@gmail.com>
2023-01-24 14:18:57 +0200

    Add Search

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,14 +1,33 @@
 const db = require("../db");
 
 exports.getHomePage = (req, res) => {
-  res.render("index", { cubes: db.cubes });
+  console.log(req.query);
+
+  const { search, from, to } = req.query;
+
+  let cubes = db.cubes;
+
+  if(search) {
+    console.log("1: ", cubes)
+    cubes = cubes.filter((cube) => cube.name.toLowerCase().includes(search.toLowerCase()));
+    console.log("2: ", cubes)
+  }
+
+  if (from) {
+    cubes = cubes.filter((cube) => cube.difficultyLevel >= from);
+  }
+
+  if (to) {
+    cubes = cubes.filter((cube) => cube.difficultyLevel <= to);
+  }
+
+  res.render("index", { cubes, search, from, to });
 };
 
 exports.getAboutPage = (req, res) => {
   res.render("about");
 };
 
-
 exports.get404 = (req, res) => {
-  res.render('404');
+  res.render("404");
 };
